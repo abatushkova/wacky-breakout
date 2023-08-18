202d6f49bca9603b38104a78dd6e50ca887ea5fb
Alena Batushkova <alena.batushkova@gmail.com>
2023-08-18 18:48:47 +0300

    add difficulty levels to config-data

---
+++ WackyBreakout/Assets/Scripts/Configuration/ConfigurationData.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A container for the configuration data
+/// </summary>
+public class ConfigurationData
+{
+    #region Fields
+
+    const string ConfigDataFileName = "ConfigurationData.csv";
+
+    // default configuration data
+    float paddleMoveUnitsPerSecond = 15;
+    float ballLifeSeconds = 10;
+    float easyBallImpulseForce = 200;
+    float easyMinSpawnSeconds = 5;
+    float easyMaxSpawnSeconds = 10;
+    float mediumBallImpulseForce = 300;
+    float mediumMinSpawnSeconds = 3;
+    float mediumMaxSpawnSeconds = 7;
+    float hardBallImpulseForce = 400;
+    float hardMinSpawnSeconds = 2;
+    float hardMaxSpawnSeconds = 5;
+    int ballsPerGame = 5;
+    int standardBlockPoints = 1;
+    int bonusBlockPoints = 2;
+    int effectBlockPoints = 5;
+    float standardBlockProbability = 0.7f;
+    float bonusBlockProbability = 0.2f;
+    float freezeBlockProbability = 0.05f;
+    float speedBlockProbability = 0.05f;
+    float freezeSeconds = 2;
+    float speedSeconds = 2;
+    float speedFactor = 2;
+
+
+    #endregion
+
+    #region Constructor
+
+    /// <summary>
+    /// Constructor
+    /// Reads configuration data from a file. If the file
+    /// read fails, the object contains default values for
+    /// the configuration data
+    /// </summary>
+    public ConfigurationData()
+    {
+        StreamReader input = null;
+        try
+        {
+            input = File.OpenText(Path.Combine(
+                Application.streamingAssetsPath, ConfigDataFileName
+            ));
+
+            // get key-value input data
+            string keys = input.ReadLine();
+            string values = input.ReadLine();
+
+            SetConfigDataFields(values);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (input != null)
+            {
+                input.Close();
+            }
+        }
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the paddle move units per second
+    /// </summary>
+    /// <value>paddle move units per second</value>
+    public float PaddleMoveUnitsPerSecond
+    {
+        get { return paddleMoveUnitsPerSecond; }
+    }
+
+    /// <summary>
+    /// Gets the number of seconds the ball lives
+    /// </summary>
+    /// <value>ball life seconds</value>
+    public float BallLifeSeconds
+    {
+        get { return ballLifeSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the impulse force to apply to move the ball
+    /// </summary>
+    /// <value>impulse force</value>
+    public float EasyBallImpulseForce
+    {
+        get { return easyBallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets the minimum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>minimum spawn seconds</value>
+    public float EasyMinSpawnSeconds
+    {
+        get { return easyMinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the maximum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>maximum spawn seconds</value>
+    public float EasyMaxSpawnSeconds
+    {
+        get { return easyMaxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the impulse force to apply to move the ball
+    /// </summary>
+    /// <value>impulse force</value>
+    public float MediumBallImpulseForce
+    {
+        get { return mediumBallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets the minimum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>minimum spawn seconds</value>
+    public float MediumMinSpawnSeconds
+    {
+        get { return mediumMinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the maximum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>maximum spawn seconds</value>
+    public float MediumMaxSpawnSeconds
+    {
+        get { return mediumMaxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the easy impulse force to apply to move the ball
+    /// </summary>
+    /// <value>impulse force</value>
+    public float HardBallImpulseForce
+    {
+        get { return hardBallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets the minimum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>minimum spawn seconds</value>
+    public float HardMinSpawnSeconds
+    {
+        get { return hardMinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the maximum number of seconds for a ball spawn
+    /// </summary>
+    /// <value>maximum spawn seconds</value>
+    public float HardMaxSpawnSeconds
+    {
+        get { return hardMaxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the number of balls per game
+    /// </summary>
+    /// <value></value>
+    public int BallsPerGame
+    {
+        get { return ballsPerGame; }
+    }
+
+    /// <summary>
+    /// Gets points of standard block
+    /// </summary>
+    /// <value></value>
+    public int StandardBlockPoints
+    {
+        get { return standardBlockPoints; }
+    }
+
+    /// <summary>
+    /// Gets points of bonus block
+    /// </summary>
+    /// <value></value>
+    public int BonusBlockPoints
+    {
+        get { return bonusBlockPoints; }
+    }
+
+    /// <summary>
+    /// Gets points of effect block
+    /// </summary>
+    /// <value></value>
+    public int EffectBlockPoints
+    {
+        get { return effectBlockPoints; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding standard block
+    /// </summary>
+    /// <value></value>
+    public float StandardBlockProbability
+    {
+        get { return standardBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding bonus block
+    /// </summary>
+    /// <value></value>
+    public float BonucBlockProbability
+    {
+        get { return bonusBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding freeze block
+    /// </summary>
+    /// <value></value>
+    public float FreezeBlockProbability
+    {
+        get { return freezeBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding speed block
+    /// </summary>
+    /// <value></value>
+    public float SpeedBlockProbability
+    {
+        get { return speedBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets duration of freeze effect
+    /// </summary>
+    /// <value></value>
+    public float FreezeSeconds
+    {
+        get { return freezeSeconds; }
+    }
+
+    /// <summary>
+    /// Gets duration of speed effect
+    /// </summary>
+    /// <value></value>
+    public float SpeedSeconds
+    {
+        get { return speedSeconds; }
+    }
+
+    /// <summary>
+    /// Gets speed factor
+    /// </summary>
+    /// <value></value>
+    public float SpeedFactor
+    {
+        get { return speedFactor; }
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void SetConfigDataFields(string csvValues)
+    {
+        string[] values = csvValues.Split(',');
+
+        // code below assumes we know the order
+        // in which values appear in input string
+        paddleMoveUnitsPerSecond = float.Parse(values[0]);
+        ballLifeSeconds = float.Parse(values[1]);
+        easyBallImpulseForce = float.Parse(values[2]);
+        easyMinSpawnSeconds = float.Parse(values[3]);
+        easyMaxSpawnSeconds = float.Parse(values[4]);
+        mediumBallImpulseForce = float.Parse(values[5]);
+        mediumMinSpawnSeconds = float.Parse(values[6]);
+        mediumMaxSpawnSeconds = float.Parse(values[7]);
+        hardBallImpulseForce = float.Parse(values[8]);
+        hardMinSpawnSeconds = float.Parse(values[9]);
+        hardMaxSpawnSeconds = float.Parse(values[10]);
+        ballsPerGame = int.Parse(values[11]);
+        standardBlockPoints = int.Parse(values[12]);
+        bonusBlockPoints = int.Parse(values[13]);
+        effectBlockPoints = int.Parse(values[14]);
+        standardBlockProbability = float.Parse(values[15]) / 100;
+        bonusBlockProbability = float.Parse(values[16]) / 100;
+        freezeBlockProbability = float.Parse(values[17]) / 100;
+        speedBlockProbability = float.Parse(values[18]) / 100;
+        freezeSeconds = float.Parse(values[19]);
+        speedSeconds = float.Parse(values[20]);
+        speedFactor = float.Parse(values[21]);
+    }
+
+    #endregion
+}
---
+++ WackyBreakout/Assets/Scripts/Configuration/ConfigurationUtils.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Provides access to configuration data
+/// </summary>
+public static class ConfigurationUtils
+{
+    #region Fields
+
+    static ConfigurationData configData;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets the paddle move units per second
+    /// </summary>
+    public static float PaddleMoveUnitsPerSecond
+    {
+        get { return configData.PaddleMoveUnitsPerSecond; }
+    }
+
+    /// <summary>
+    /// Gets seconds the ball lives
+    /// </summary>
+    public static float BallLifeSeconds
+    {
+        get { return configData.BallLifeSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the ball impulse force
+    /// </summary>
+    public static float BallImpulseForce
+    {
+        get { return DifficultyUtils.BallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets min seconds for a ball to spawn
+    /// </summary>
+    public static float MinSpawnSeconds
+    {
+        get { return DifficultyUtils.MinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets max seconds for a ball to spawn
+    /// </summary>
+    public static float MaxSpawnSeconds
+    {
+        get { return DifficultyUtils.MaxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the number of balls per game
+    /// </summary>
+    public static int BallsPerGame
+    {
+        get { return configData.BallsPerGame; }
+    }
+
+    /// <summary>
+    /// Gets points of standard block
+    /// </summary>
+    public static int StandardBlockPoints
+    {
+        get { return configData.StandardBlockPoints; }
+    }
+
+    /// <summary>
+    /// Gets points of bonus block
+    /// </summary>
+    public static int BonusBlockPoints
+    {
+        get { return configData.BonusBlockPoints; }
+    }
+
+    /// <summary>
+    /// Gets points of effect block
+    /// </summary>
+    public static int EffectBlockPoints
+    {
+        get { return configData.EffectBlockPoints; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding standard block
+    /// </summary>
+    public static float StandardBlockProbability
+    {
+        get { return configData.StandardBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding bonus block
+    /// </summary>
+    public static float BonucBlockProbability
+    {
+        get { return configData.BonucBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding freeze block
+    /// </summary>
+    public static float FreezeBlockProbability
+    {
+        get { return configData.FreezeBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets probability of adding speed block
+    /// </summary>
+    public static float SpeedBlockProbability
+    {
+        get { return configData.SpeedBlockProbability; }
+    }
+
+    /// <summary>
+    /// Gets duration of freeze effect
+    /// </summary>
+    public static float FreezeSeconds
+    {
+        get { return configData.FreezeSeconds; }
+    }
+
+    /// <summary>
+    /// Gets duration of speed effect
+    /// </summary>
+    public static float SpeedSeconds
+    {
+        get { return configData.SpeedSeconds; }
+    }
+
+    /// <summary>
+    /// Gets speed factor
+    /// </summary>
+    public static float SpeedFactor
+    {
+        get { return configData.SpeedFactor; }
+    }
+
+    #endregion
+
+    #region Difficulty properties
+
+    /// <summary>
+    /// Gets the ball impulse force
+    /// </summary>
+    public static float EasyBallImpulseForce
+    {
+        get { return configData.EasyBallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets min seconds for a ball to spawn
+    /// </summary>
+    public static float EasyMinSpawnSeconds
+    {
+        get { return configData.EasyMinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets max seconds for a ball to spawn
+    /// </summary>
+    public static float EasyMaxSpawnSeconds
+    {
+        get { return configData.EasyMaxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the ball impulse force
+    /// </summary>
+    public static float MediumBallImpulseForce
+    {
+        get { return configData.MediumBallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets min seconds for a ball to spawn
+    /// </summary>
+    public static float MediumMinSpawnSeconds
+    {
+        get { return configData.MediumMinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets max seconds for a ball to spawn
+    /// </summary>
+    /// <value></value>
+    public static float MediumMaxSpawnSeconds
+    {
+        get { return configData.MediumMaxSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets the ball impulse force
+    /// </summary>
+    public static float HardBallImpulseForce
+    {
+        get { return configData.HardBallImpulseForce; }
+    }
+
+    /// <summary>
+    /// Gets min seconds for a ball to spawn
+    /// </summary>
+    public static float HardMinSpawnSeconds
+    {
+        get { return configData.HardMinSpawnSeconds; }
+    }
+
+    /// <summary>
+    /// Gets max seconds for a ball to spawn
+    /// </summary>
+    public static float HardMaxSpawnSeconds
+    {
+        get { return configData.HardMaxSpawnSeconds; }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Initializes the configuration utils
+    /// </summary>
+    public static void Initialize()
+    {
+        configData = new ConfigurationData();
+    }
+
+    #endregion
+}
